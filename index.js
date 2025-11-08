8d929ba40ee5df30b65a34f00b9b80640bbbfc98
sajjadursohag <sajjadursub07@gmail.com>
2025-11-08 23:41:23 +0600

    added mongodb atlas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +8,38 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+
+
+// bookhavendbUser
+// ZMNPWlfIvhnyCLGH
+
+const uri = "mongodb+srv://bookhavendbUser:ZMNPWlfIvhnyCLGH@cluster0.mmla0te.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
 app.get("/", (req, res) => {
   res.send("The Book Haven Server is Running");
 });
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`The Book Haven Server is Running on Port: ${port}`);
